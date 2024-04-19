4bab5523dd680cf67a8fcdfe18c895bcd7ce10b6
Cloyd Van <abcd123.letters@gmail.com>
2024-04-20 00:32:39 +0800

    Common User attributes
    I now defined the attributes to represent a User entity

--- src/main/java/com/userservice/user/User.java
+++ src/main/java/com/userservice/user/User.java
@@ -1,4 +1,26 @@
 package com.userservice.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "USERS")
 public class User {
+
+    @Id
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
 }
