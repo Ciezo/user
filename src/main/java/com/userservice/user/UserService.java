4a0817fd40955af71d508ad1fc19d8798278d4d9
Cloyd Van <abcd123.letters@gmail.com>
2024-04-20 04:32:17 +0800

    UserService updates

--- src/main/java/com/userservice/user/UserService.java
+++ src/main/java/com/userservice/user/UserService.java
@@ -1,4 +1,19 @@
 package com.userservice.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Optional<User> getAllUserDetailsByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user;
+    }
+
 }
