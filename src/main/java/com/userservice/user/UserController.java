6d2f704d33119945a49dc5324fb210f3ce89b5e2
Cloyd Van <abcd123.letters@gmail.com>
2024-04-23 01:12:23 +0800

    UserController
    Get all user details using email (String)

--- src/main/java/com/userservice/user/UserController.java
+++ src/main/java/com/userservice/user/UserController.java
@@ -1,4 +1,39 @@
 package com.userservice.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user-service/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * This returns a list of all `UserDetails` based on the
+     * `User` model defined. Moreover, a username defined here
+     * is an email.
+     *
+     * Email addresses are unique hence, it is defined as an attribute
+     * to unique identify the names of our users in the database.
+     * @param email attribute from the `User`
+     * @return UserDetails attributes
+     */
+    @GetMapping("/un/{username}")
+    public ResponseEntity<Optional<User>> getAllUserDetailsByUsername(
+            @PathVariable(value = "username") String email
+    ) {
+        return new ResponseEntity<>(
+                userService.getAllUserDetailsByEmail(email),
+                HttpStatus.OK
+        );
+    }
 }
