29f488c9ce5fe72e600734f6f44f7642babb16e2
Cloyd Van <abcd123.letters@gmail.com>
2024-04-23 15:35:46 +0800

    DemoController
    1. Added a test endpoint which requires Authentication
    2. Added another test endpoint which requires NO Authentication

--- src/main/java/com/userservice/user/demo/DemoController.java
+++ src/main/java/com/userservice/user/demo/DemoController.java
@@ -1,4 +1,30 @@
 package com.userservice.user.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user-service/v1/demo")
 public class DemoController {
+
+    /**
+     * This endpoint requires Authentication
+     * @return `true` if a user is logged-in with an assigned JWT token
+     */
+    @GetMapping("/auth/test")
+    public ResponseEntity<String> test_auth() {
+        return ResponseEntity.ok("true");
+    }
+
+    /**
+     * This endpoint can be used with no Authentication
+     * @return `true` always even if it has no authentication
+     */
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("true");
+    }
+
 }
