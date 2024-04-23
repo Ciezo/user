205fdb3fcd13335d802c22765b23af8db8c64a5b
Cloyd Van <abcd123.letters@gmail.com>
2024-04-23 16:28:44 +0800

    Authentication
    I still need some time to clarify this along with the Security Configuration

--- src/main/java/com/userservice/user/auth/AuthenticationResponse.java
+++ src/main/java/com/userservice/user/auth/AuthenticationResponse.java
@@ -1,4 +1,16 @@
 package com.userservice.user.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationResponse {
+
+    private String token;
+
 }
--- src/main/java/com/userservice/user/auth/AuthenticationService.java
+++ src/main/java/com/userservice/user/auth/AuthenticationService.java
@@ -1,4 +1,16 @@
 package com.userservice.user.auth;
 
+import com.userservice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder pwdEncoder;
+
+
 }
