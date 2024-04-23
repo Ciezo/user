619351452da6724da9d5ff1a4d73a8c36b593a0a
Cloyd Van <abcd123.letters@gmail.com>
2024-04-23 16:50:13 +0800

    Security Context Holder
    Setting Up User to be authenticated using the username and password as credentials

--- src/main/java/com/userservice/user/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/userservice/user/jwt/JWTAuthenticationFilter.java
@@ -14,7 +14,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -55,5 +59,34 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         jwt = getAuthRequestHeader.substring(7);            // Remove the Bearer prefix
         // Assign the username as sub in the Claims
         username = jwtService.extractUsername(jwt);
+
+        /** Check if the User is authenticated and connected with authorities */
+        if(username != null &&
+                // If the user is not authenticated then, proceed to use the username and password
+                // credentials to log our user in the application
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Load-up UserDetails using the assigned username extracted from the Claim
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            // Validate the generated JWT token
+            if( jwtService.isTokenGenValid(jwt, userDetails) ) {
+                System.out.println("jwt valid");
+                // Fill in the Security Context
+                // These are the required values since
+                // we are implementing the UserDetails service
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authToken.setDetails( new WebAuthenticationDetailsSource().buildDetails(request) );
+                /** Set Authentication Context once the user is authenticated with a VALID JWT token */
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        /** Continue filtering requests and responses */
+        filterChain.doFilter(request, response);
     }
 }
