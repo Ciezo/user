a0c151e8b5f1376a5b817777bc134477184d0b74
Cloyd Van <abcd123.letters@gmail.com>
2024-04-24 15:24:20 +0800

    Authentication Controller
    To sign-in and sign-up Users with the Authentication Service

--- src/main/java/com/userservice/user/auth/AuthenticationController.java
+++ src/main/java/com/userservice/user/auth/AuthenticationController.java
@@ -1,4 +1,43 @@
 package com.userservice.user.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    /**
+     * Sign-up user
+     * @param request firstname, lastname, email, password
+     * @return a JWT token and saved User into the MySQL database
+     */
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(
+                authService.register(request)
+        );
+    }
+
+    /**
+     * Sign-in user
+     * @param request username and password credentials
+     * @return a JWT token from an existing User in the database
+     */
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate (
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(
+                authService.authenticate(request)
+        );
+    }
 }
+
