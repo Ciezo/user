26c577e0916a722732c760aab9fa677869ecdff8
Cloyd Van <abcd123.letters@gmail.com>
2024-04-25 12:36:05 +0800

    Update mapping for Authentication Controller
    Corrected some path naming

--- src/main/java/com/userservice/user/auth/AuthenticationController.java
+++ src/main/java/com/userservice/user/auth/AuthenticationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/user-service/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
