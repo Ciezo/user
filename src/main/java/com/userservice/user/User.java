42c8b549fc8eb2c540b4ae2841549199cc7e2618
Cloyd Van <abcd123.letters@gmail.com>
2024-04-25 13:09:54 +0800

    Increment pk id

--- src/main/java/com/userservice/user/User.java
+++ src/main/java/com/userservice/user/User.java
@@ -1,10 +1,7 @@
 package com.userservice.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,6 +18,7 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private long id;
     private String firstname;
