9d7eb32e918314bf0c97e270813b45b09ffd4ecb
Cloyd Van <abcd123.letters@gmail.com>
2024-04-29 13:42:59 +0800

    disable SSL for dev and testing purposes
    I am in localhost only

--- src/main/java/com/userservice/user/UserController.java
+++ src/main/java/com/userservice/user/UserController.java
@@ -3,15 +3,13 @@ package com.userservice.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user-service/v1/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
